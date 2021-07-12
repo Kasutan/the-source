0f62aebd5db0ea0afa1311465d74a65ef1174fb6
Kasutan <contact@kasutan.pro>
2021-07-12 17:41:30 +0200

    Thème footer

--- inc/site-footer.php
+++ inc/site-footer.php
@@ -8,9 +8,10 @@
 */
 add_action( 'tha_footer_bottom', 'kasutan_copyright' );
 function kasutan_copyright() {
+	echo '<a class="backtotop" href="#main-content"><span class="screen-reader-text">Back to top</span>' . kasutan_picto( array( 'icon' => 'backtotop' , 'size' => false) ) . '</a>';
+
 	echo '<div class="copyright">';
-		printf('<span class="annee">%s</span>',date('Y'));
-		printf('<span class="titre">%s</span>',get_option('blogname'));
+		printf('<span class="annee">&copy; %s %s</span>',date('Y'), get_option('blogname'));
 		if( has_nav_menu( 'footer-legal' ) ) {
 			wp_nav_menu( array( 'theme_location' => 'footer-legal', 'menu_id' => 'footer-legal', 'container_class' => 'nav-footer-legal' ) );
 		}
