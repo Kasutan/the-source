08a7df707efa88ffbf757141f8544730c7997ad5
Kasutan <contact@kasutan.pro>
2021-07-18 18:10:22 +0200

    My account page choisie en option BO

--- inc/pmp/my-account.php
+++ inc/pmp/my-account.php
@@ -2,24 +2,13 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 function kasutan_is_account_child_page() {
-	$response=false;
 	$parent_id=wp_get_post_parent_id(get_the_ID());
-	if(function_exists('pmpro_getOption')) {
-		$account_page_id=pmpro_getOption('account_page_id');
-		if($account_page_id==$parent_id) {
-			$response=true;
-		}
-	}
-	return $response;
+	$account_page_id=kasutan_get_page_ID('my_account');
+
+	return $account_page_id==$parent_id;
 }
 
 function kasutan_is_account_page(){
-	$response=false;
-	if(function_exists('pmpro_getOption')) {
-		$account_page_id=pmpro_getOption('account_page_id');
-		if($account_page_id==get_the_ID()){
-			$response=true;
-		}
-	}
-	return $response;
+	$account_page_id=kasutan_get_page_ID('my_account');
+	return $account_page_id==get_the_ID();
 }
