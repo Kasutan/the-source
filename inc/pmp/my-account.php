13b350c657bf88f96f563d690754a3c590d8a373
Kasutan <contact@kasutan.pro>
2021-07-23 17:33:15 +0200

    Fin My membership + style My billing information

--- inc/pmp/my-account.php
+++ inc/pmp/my-account.php
@@ -34,4 +34,131 @@ function kasutan_change_password() {
 	return ob_get_clean();
 }
 
-//TODO ajouter une colonne au tableau My invoices avec le lien vers la facture PDF + désactiver le lien sur la date -> imiter le shortode my account shortcodes\pmpro_account.php (ligne 409 et suivantes)
+//Ajouter une colonne au tableau My invoices avec le lien vers la facture PDF -> imiter le shortode my account shortcodes\pmpro_account.php (ligne 409 et suivantes)
+
+add_shortcode( 'my-invoices', 'kasutan_my_invoices');
+function kasutan_my_invoices() {
+	ob_start();
+
+	if(function_exists('pmpro_get_element_class')) {
+		echo '<div id="pmpro_account-invoices">';
+		global $wpdb, $current_user;
+		$invoices = $wpdb->get_results("SELECT *, UNIX_TIMESTAMP(CONVERT_TZ(timestamp, '+00:00', @@global.time_zone)) as timestamp FROM $wpdb->pmpro_membership_orders WHERE user_id = '$current_user->ID' AND status NOT IN('review', 'token', 'error') ORDER BY timestamp DESC LIMIT 6");
+
+		?>
+		<div id="pmpro_account-invoices" class="<?php echo pmpro_get_element_class( 'pmpro_box', 'pmpro_account-invoices' ); ?>">
+
+			<h3>My invoices</h3>
+
+			<table class="<?php echo pmpro_get_element_class( 'pmpro_table' ); ?>" width="100%" cellpadding="0" cellspacing="0" border="0">
+
+				<thead>
+
+					<tr>
+
+						<th><?php _e("Date", 'paid-memberships-pro' ); ?></th>
+
+						<th><?php _e("Amount", 'paid-memberships-pro' ); ?></th>
+
+						<th><?php _e("Status", 'paid-memberships-pro'); ?></th>
+
+						<th>Download</th>
+
+					</tr>
+
+				</thead>
+
+				<tbody>
+
+				<?php
+
+					$count = 0;
+
+					foreach($invoices as $invoice)
+
+					{
+
+						if($count++ > 4)
+
+							break;
+
+
+
+						//get an member order object
+
+						$invoice_id = $invoice->id;
+
+						$invoice = new MemberOrder;
+
+						$invoice->getMemberOrderByID($invoice_id);
+
+						$invoice->getMembershipLevel();
+
+
+
+						if ( in_array( $invoice->status, array( '', 'success', 'cancelled' ) ) ) {
+
+							$display_status = __( 'Paid', 'paid-memberships-pro' );
+
+						} elseif ( $invoice->status == 'pending' ) {
+
+							// Some Add Ons set status to pending.
+
+							$display_status = __( 'Pending', 'paid-memberships-pro' );
+
+						} elseif ( $invoice->status == 'refunded' ) {
+
+							$display_status = __( 'Refunded', 'paid-memberships-pro' );
+
+						}
+						
+						$order_code = $invoice->code;
+						$order_data = pmpropdf_get_order_by_code( $order_code );
+						$download_url='#';
+						if ( ! empty( $order_data ) ) {	
+							if ( $current_user->ID === intval( $order_data[0]->user_id ) || current_user_can( 'manage_options' ) ){
+								if( file_exists( pmpropdf_get_invoice_directory_or_url() . pmpropdf_generate_invoice_name( $order_code ) ) ) {
+									$invoice_name = pmpropdf_generate_invoice_name( $order_code );
+									$download_url = esc_url( pmpropdf_get_invoice_directory_or_url( true ) . $invoice_name );
+									$access_key = pmpropdf_get_rewrite_token();
+
+									$download_url .= "?access=$access_key";
+								}
+							}
+						}
+						?>
+
+						<tr id="pmpro_account-invoice-<?php echo $invoice->code; ?>">
+
+							<td><?php echo date_i18n(get_option("date_format"), $invoice->getTimestamp())?></a></td>
+
+							<td><?php echo pmpro_escape_price( pmpro_formatPrice($invoice->total) ); ?></td>
+
+							<td><?php echo $display_status; ?></td>
+
+							<td><?php printf('<a href="%s" class="download">Invoice [PDF]</a>',$download_url); ?></td>
+
+						</tr>
+
+						<?php
+
+					}
+
+				?>
+
+				</tbody>
+
+			</table>
+
+			<?php if($count == 6) { ?>
+
+				<div class="<?php echo pmpro_get_element_class( 'pmpro_actionlinks' ); ?>"><a id="pmpro_actionlink-invoices" href="<?php echo pmpro_url("invoice"); ?>"><?php _e("View All Invoices", 'paid-memberships-pro' );?></a></div>
+
+			<?php } ?>
+
+		</div> <!-- end pmpro_account-invoices -->
+		</div>
+		<?php
+	}
+	return ob_get_clean();
+}
