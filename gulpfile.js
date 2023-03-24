fe85836ae6475719c2ed2a3e6d3dbf2d961bee0b
Kasutan <contact@kasutan.pro>
2023-03-24 20:36:46 +0100

    Upgrade gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Grab our gulp packages
 const gulp  = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
@@ -16,10 +16,7 @@ gulp.task('styles', function() {
     return gulp.src('sass/*.scss')
         .pipe(sourcemaps.init()) // Start Sourcemaps
         .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        .pipe(autoprefixer())
 		//.pipe(cleanCSS({compatibility: '*'}))
         .pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
 		.pipe(gulp.dest('./'));
@@ -31,27 +28,24 @@ gulp.task('blocks-styles', function() {
     
 	return gulp.src('partials/blocks/*/*.scss')
 		.pipe(sass())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
+		.pipe(autoprefixer())
 		//.pipe(cleanCSS({compatibility: '*'}))
 		.pipe(gulp.dest(function (file) {
 			return file.base;
 		}));
 });
 
-gulp.task('all-styles', ['styles','blocks-styles']);
-gulp.task('styles-all', ['styles','blocks-styles']);
+gulp.task('all-styles', gulp.series('styles','blocks-styles'));
+gulp.task('styles-all',  gulp.series('styles','blocks-styles'));
 
 // Watch files for changes (without Browser-Sync)
 gulp.task('watch', function() {
 
 	// Watch main theme .scss files
-	gulp.watch('sass/*/*.scss', ['styles']);
+	gulp.watch('sass/*/*.scss', gulp.series('styles'));
 
 	// Watch blocks .scss files
-	gulp.watch('partials/blocks/*/*.scss', ['blocks-styles']);
+	gulp.watch('partials/blocks/*/*.scss', gulp.series('blocks-styles'));
 
 
 
