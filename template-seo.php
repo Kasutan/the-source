1de15b12ab2b976c85e3133681342a80c1b2da86
Kasutan <contact@kasutan.pro>
2023-04-03 11:49:08 +0200

    Template pour pages SEO

---
+++ template-seo.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Template SEO */
+
+// Breadcrumbs
+add_action( 'ea_content_header_after', 'kasutan_fil_ariane', 5 );
+
+// Build the page
+require get_template_directory() . '/index.php';
+
+
+
+
+
